e6ec7d7392cb08a40d2d6eb9e17b8620c72de79a
weissrot <93322291+weissrot@users.noreply.github.com>
2022-01-20 13:19:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def reply():
     number = request.form.get("From")
     response = MessagingResponse()
     user = users.find_one({"number": number})
-    if bool(user) == False:
+    if "Hi" in text:
         response.message("Hi, thanks for contacting *The Cutest Bot*.\nYou can choose from one of the options below: "
                     "\n\n*Type*\n\n 1️⃣ talking about *relationship* 😘 \n 2️⃣ talking about *work* 😅 \n 3️⃣ talking about *sports* 😎 \n 4️⃣ "
                     "talking about *happiness* 😀")
@@ -34,9 +34,6 @@ def reply():
             response.message(
                 "You can message me through phone or e-mail.\n\n*Phone*: 991234 56789 \n*E-mail* : contact@theredvelvet.io")
         elif option == 2:
-            response.message("You have entered *working mode*.")
-            users.update_one(
-                {"number": number}, {"$set": {"status": "ordering"}})
             response.message(
                 "What do you want to talk about?")
         elif option == 3:
@@ -45,19 +42,6 @@ def reply():
             response.message("your smile lightens my day")
         else:
             response.message("Please enter a valid response")
-    elif user["status"] == "ordering":
-        if "handsome" in text:
-            users.update_one(
-                {"number": number}, {"$set": {"status": "main"}})
-            response.message("Frederik")
-        elif "hi" in text
-            users.update_one(
-                {"number": number}, {"$set": {"status": "main"}})
-            response.message("Good morning! What do you plan to do today?")
-        else:
-            users.update_one(
-                {"number": number}, {"$set": {"status": "main"}})
-            response.message("Let me think about it")
 
     users.update_one({"number": number}, {"$push": {"messages": {"text": text}}})
     return str(response)
